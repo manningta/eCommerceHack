ec49cbd8546f773d00666587816dd66198ba7531
Steve Osacky <steveosacky@gmail.com>
2019-05-22 12:15:35 -0400

    change

--- eCommerceHack.API/Controllers/ShoppingCartController3.cs
+++ eCommerceHack.API/Controllers/ShoppingCartController3.cs
@@ -1,8 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerceHack.API.Models;
+using eCommerceHack.Service.Models;
+using Microsoft.AspNetCore.Mvc;
+using eCommerceHack.Service.ShoppingCart;
 
 namespace eCommerceHack.API.Controllers
 {
     public partial class ShoppingCartController : Controller
     {
+        private eCommerceContext _context;
+
+        public ShoppingCartController(eCommerceContext context)
+        {
+            _context = context;
+        }
+        
+        public IActionResult GetCart([FromBody] ShoppingCartDto cartDto)
+        {
+            var cartDto = Service.ShoppingCart.ShoppingCart.GetCart(item, _context);
+
+            if (cartDto == null) return new OkObjectResult(new { error = "No items found in cart" });
+
+            return new OkObjectResult(cartDto);
+        }
     }
+
+
 }
--- eCommerceHack.Service/ShoppingCart/ShoppingCart3.cs
+++ eCommerceHack.Service/ShoppingCart/ShoppingCart3.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using eCommerceHack.API.Models;
+using eCommerceHack.Service.Models;
+using System.Linq;
 
 namespace eCommerceHack.Service.ShoppingCart
 {
     public partial class ShoppingCart3
-    {
+    {       
+        public static ShoppingCartItem GetCart(GetItem item, eCommerceContext _context)
+        {
+            if (!_context.ShoppingCart.Any(x => x.ShoppingCartID = item.ShopppingCartID)) return null;
+
+            if (item.Quantity <= 0) return null;
+
+            //var cart = _context.
+
+            return GetCart;
+        }
+
     }
 }
