2c6fa8dd2d5a082d769cf4422f15e03147a2aeaf
Grusak, Steve <Steve.Grusak@crowe.com>
2019-05-22 09:28:50 -0400

    Initial DB Table

---
+++ eCommerceDB/Tables/Product.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+    [ProductId] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [Description] VARCHAR(100) NOT NULL DEFAULT '', 
+    [Price] DECIMAL(19, 4) NOT NULL, 
+    [Shipping] DECIMAL(19, 4) NOT NULL
+)
