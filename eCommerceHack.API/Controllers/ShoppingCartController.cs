b59b43bfbd78b8bd92849b68dd9ea8652211593e
TravisManning <ManningTA@crowe.com>
2019-05-22 09:35:24 -0400

    added controller with 2 files as partial to prevent merge conflicts

---
+++ eCommerceHack.API/Controllers/ShoppingCartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceHack.API.Controllers
+{
+    public partial class ShoppingCartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
