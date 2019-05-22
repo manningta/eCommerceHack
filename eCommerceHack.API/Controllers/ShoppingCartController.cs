c84c82e53e58d3c45f88181429aff8a767f04b31
TravisManning <ManningTA@crowe.com>
2019-05-22 10:47:35 -0400

    DI for context

--- eCommerceHack.API/Controllers/ShoppingCartController.cs
+++ eCommerceHack.API/Controllers/ShoppingCartController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerceHack.API.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace eCommerceHack.API.Controllers
 {
     public partial class ShoppingCartController : Controller
     {
+        private eCommerceContext _context;
+
+        public ShoppingCartController(eCommerceContext context)
+        {
+            _context = context;
+        }
     }
 
     public class AddItemDto
